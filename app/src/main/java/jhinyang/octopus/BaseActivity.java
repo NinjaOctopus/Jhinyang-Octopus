04a42d3350e7c7ebcb66ed148fcf49e9767586bd
Ashutosh <ashutoshsmartcool@gmail.com>
2018-08-13 15:51:51 +0530

    baseactivity lifecycle

--- app/src/main/java/jhinyang/octopus/BaseActivity.java
+++ app/src/main/java/jhinyang/octopus/BaseActivity.java
@@ -1,4 +1,38 @@
 package jhinyang.octopus;
 
-public class BaseActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        BaseActivity.super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }

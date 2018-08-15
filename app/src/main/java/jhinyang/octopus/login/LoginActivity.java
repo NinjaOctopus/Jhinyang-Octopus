bdf94a366e804e28894d2bb7c8868a86cc06b3bd
Ashutosh <ashutoshsmartcool@gmail.com>
2018-08-15 22:18:10 +0530

    support for fabric and crashlytics

--- app/src/main/java/jhinyang/octopus/login/LoginActivity.java
+++ app/src/main/java/jhinyang/octopus/login/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -13,6 +15,7 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 
 import butterknife.BindView;
@@ -31,6 +34,7 @@ public class LoginActivity extends BaseActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Answers(), new Crashlytics());
 
         setContentView(R.layout.login_splash);
         ButterKnife.bind(LoginActivity.this);
